8bc0346e2450683b0250e92b54147985e5a7a36f
FalseCodee <falsecodee@gmail.com>
2022-01-14 19:38:38 -0500

    50,000

--- src/me/falsecode/numbers/Main.java
+++ src/me/falsecode/numbers/Main.java
@@ -4,7 +4,7 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
-        printOutEnums(1, 10000);
+        printOutEnums(10001, 50000);
     }
 
     public static void printOutEnums(final int start, final int end) {
